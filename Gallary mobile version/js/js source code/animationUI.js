c67722ad652383930eb2340e45b029cc949306b7
Кирилл <mr.glazki@mail.ru>
2019-12-18 18:07:37 +0900

    Created folders

---
+++ Gallary mobile version/js/js source code/animationUI.js
@@ -0,0 +1,78 @@
+document.addEventListener('click', checkButton);
+
+let posYSlider = 0;
+let posYLocation = 0;
+
+function checkButton(event) {
+	
+	if (errorRadar(event.target)) {
+
+		return;
+
+	};
+
+	let dataType = event.target.closest('button').dataset.direction;
+
+	switch (dataType) {
+
+		case 'up': 
+			slideUp();
+		break;
+
+		case 'down': 
+			slideDown();
+		break;
+
+	};
+
+}
+
+function errorRadar(e) {
+
+	let button = e.closest('button');
+	
+	if (!button) {
+
+		return true;
+
+	} else if (button.dataset.direction == '') {
+
+		return true;
+
+	};
+
+	return false;
+
+};
+
+function slideUp() {
+
+	if (posYSlider <= 0) {
+
+		return;
+
+	};
+
+	let slider = document.querySelector('.main-window');
+
+	posYSlider -= document.documentElement.clientHeight;
+
+	slider.style.transform = `translateY(-${posYSlider}px)`;  
+
+};
+
+function slideDown() {
+
+	if (posYSlider >= (document.documentElement.clientHeight * 3)) {
+
+		return;
+
+	};
+
+	let slider = document.querySelector('.main-window');
+
+	posYSlider += document.documentElement.clientHeight;
+
+	slider.style.transform = `translateY(-${posYSlider}px)`;  
+
+};
