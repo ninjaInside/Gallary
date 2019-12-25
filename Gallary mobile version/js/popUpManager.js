0c52ba18072d2e1b1005c79fac41f674ca1c6c09
Кирилл <mr.glazki@mail.ru>
2019-12-25 20:17:47 +0900

    pop up fix

--- Gallary mobile version/js/popUpManager.js
+++ Gallary mobile version/js/popUpManager.js
@@ -1,87 +1,45 @@
-let dataText = [
+let x = document.documentElement.clientWidth / 2;
+let y = document.documentElement.clientHeight / 2;
+
+document.addEventListener('DOMContentLoaded', hidePopUp);
+document.addEventListener('click', closePopUp);
+
+function hidePopUp() {
 	
-	`
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-		Eos sequi amet pariatur accusamus, assumenda, iste ut 
-		animi dicta sunt error, ab dolore quidem, 
-		repellat nostrum. 0
-	`,
+	let arrayForImg = document.querySelectorAll('.pop-up');
 
-	`
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-		Eos sequi amet pariatur accusamus, assumenda, iste ut 
-		animi dicta sunt error, ab dolore quidem, 
-		repellat nostrum. 1
-	`,
+	for (let elem of arrayForImg) {
 
-	`
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-		Eos sequi amet pariatur accusamus, assumenda, iste ut 
-		animi dicta sunt error, ab dolore quidem, 
-		repellat nostrum. 2
-	`,
+		elem.style.display = 'none';
 
-	`
-		Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-		Eos sequi amet pariatur accusamus, assumenda, iste ut 
-		animi dicta sunt error, ab dolore quidem, 
-		repellat nostrum. 3
-	`		
+	}
 
-];
+};
 
-document.addEventListener('click', checkButton);
+let showPopUpButton = document.querySelector('#show-text');
 
-function checkButton(e) {
-
-	if (e.target.nodeName != 'BUTTON') {
-
-		return;
-
-	} else if (e.target.dataset.type != 'pop') {
-
-		return;
-
-	};
-
-	showPopUp();
-
-}
+showPopUpButton.addEventListener('click', showPopUp);
 
 function showPopUp() {
+	
+	let index = document.elementFromPoint(x, y);
+		index = index.dataset.index;
+		index = +index;
 
-	let x = document.documentElement.clientWidth / 2;
- 	let y = document.documentElement.clientHeight / 2;
- 	let elemForText = document.elementFromPoint(x, y); 
- 		elemForText = elemForText.closest('section')
- 		elemForText = +elemForText.dataset.location;
+	let currentPopUp = document.querySelectorAll('.pop-up');
+		currentPopUp = currentPopUp[index];
 
- 	let newElemIconButton = document.createElement('button');
- 		newElemIconButton.classList.add('pop-up-button');
+	currentPopUp.style.display = 'flex';
 
- 	let newElemSpan = document.createElement('span');
- 		newElemSpan.insertAdjacentElement('afterbegin', newElemIconButton);
- 		newElemSpan.append(dataText[elemForText]);
-
-  	let newElemDiv = document.createElement('div');
- 		newElemDiv.classList.add('pop-up');
- 		newElemDiv.insertAdjacentElement('afterbegin', newElemSpan);
-
- 	document.body.insertAdjacentElement('afterbegin', newElemDiv);
-
- 	let closeTextWindow = document.querySelector('.pop-up-button');
-
- 	closeTextWindow.addEventListener('click', closePopUp);
-}
+};
 
 function closePopUp(e) {
-	
-	let popUp = document.querySelector('.pop-up');
 
-	popUp.remove();
+	if (e.target.nodeName != 'BUTTON') return;
+	if (e.target.dataset.type != 'close') return;
 
-	e.target.removeEventListener('click', closePopUp);
+	let target = e.target.closest('div');
+
+	target.style.display = 'none';
 
 }
-
-
