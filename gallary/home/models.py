51c6dfea1ce7b6fc635eea6a51bf8e66612445e2
artemowa <loskan2017@yandex.ru>
2019-12-27 00:00:13 +0900

    PEP8 home model fix

--- gallary/home/models.py
+++ gallary/home/models.py
@@ -5,7 +5,7 @@ class Place(models.Model):
     name = models.CharField('Название места', unique=True, max_length=10)
     url = models.URLField('Ссылка на google maps', max_length=500)
     text = models.TextField('Описание места', max_length=450,
-            help_text='Максимальная длина: 450 символов')
+        help_text='Максимальная длина: 450 символов')
     image = models.ImageField('Изображение места')
 
     def __str__(self):
