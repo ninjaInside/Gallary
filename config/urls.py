d898b724428d5a0335ad2af8d2be316110b64da6
artemowa <loskan2017@yandex.ru>
2019-12-26 23:51:54 +0900

    delete static function in main URLConf

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import RedirectView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('', RedirectView.as_view(url='callforward/')),
     path('callforward/', include('callforward.urls')),
     path('home/', include('home.urls')),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
