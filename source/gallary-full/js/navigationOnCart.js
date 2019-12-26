9fe1563214747365ea6f8be71c9f04480f2a9ab8
artemowa <loskan2017@yandex.ru>
2019-12-27 01:07:29 +0900

    django project + source code html/css/js

---
+++ source/gallary-full/js/navigationOnCart.js
@@ -0,0 +1,31 @@
+let posYWindow;
+let posXWindow;
+let params;
+
+window.onload = function() {
+
+	posYWindow = document.documentElement.clientHeight / 2 - 200;
+	posXWindow = document.documentElement.clientWidth / 2 - 400;
+	params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,
+			width=800px,height=400px,left=${posXWindow},top=${posYWindow}`;
+
+};
+
+
+document.querySelector('.show-map').addEventListener('click', preloadPopUp);
+
+function preloadPopUp(e) {
+
+	let posXWindow = document.documentElement.clientWidth / 2;
+	let numberOfData = document.elementFromPoint(posXWindow, posYWindow);
+	let dataLocation = numberOfData.dataset.location;
+
+	openPopUp(dataLocation);
+
+};
+
+function openPopUp(locate) {
+
+	window.open(`${locate}`, 'test', params);
+
+};
